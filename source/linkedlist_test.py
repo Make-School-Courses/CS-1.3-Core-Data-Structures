cf7b97846cc70012c4b95475c20e8e380c802b47
Avery Durrant <avery246813579@gmail.com>
2017-11-01 13:02:34 -0700

    With Sam fixed Linked List Insert Test
    You forgot to update the size when asserting.

--- source/linkedlist_test.py
+++ source/linkedlist_test.py
@@ -92,15 +92,15 @@ class LinkedListTest(unittest.TestCase):
         ll.insert_at_index(0, 'A')  # prepend('A')
         assert ll.head.data == 'A'  # new head (at index 0)
         assert ll.tail.data == 'B'  # unchanged (now at index 1)
-        assert ll.size == 3
+        assert ll.size == 2
         ll.insert_at_index(2, 'D')  # append('D')
         assert ll.head.data == 'A'  # unchanged (at index 0)
         assert ll.tail.data == 'D'  # new tail (now at index 2)
-        assert ll.size == 2
+        assert ll.size == 3
         ll.insert_at_index(2, 'C')  # insert 'C' between 'B' and 'D'
         assert ll.head.data == 'A'  # unchanged (at index 0)
         assert ll.tail.data == 'D'  # unchanged (now at index 3)
-        assert ll.size == 2
+        assert ll.size == 4
         with self.assertRaises(ValueError):
             ll.insert_at_index(5, 'X')  # index too high
         with self.assertRaises(ValueError):
