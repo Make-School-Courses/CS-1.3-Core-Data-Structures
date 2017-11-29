08ec5ba7ffcc057897ff5804ba29baa1e7f70e7d
Alan Davis <neptunius@gmail.com>
2017-11-29 15:33:33 -0800

    Add partition and quick sort algorithms to sorting template

--- source/sorting.py
+++ source/sorting.py
@@ -69,6 +69,32 @@ def merge_sort(items):
     # TODO: Merge sorted halves into one list in sorted order
 
 
+def partition(items, low, high):
+    """Return index `p` after in-place partitioning given items in range
+    `[low...high]` by choosing a pivot (TODO: document your method here) from
+    that range, moving pivot into index `p`, items less than pivot into range
+    `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Choose a pivot any way and document your method in docstring above
+    # TODO: Loop through all items in range [low...high]
+    # TODO: Move items less than pivot into front of range [low...p-1]
+    # TODO: Move items greater than pivot into back of range [p+1...high]
+    # TODO: Move pivot item into final position [p] and return index p
+
+
+def quick_sort(items, low=None, high=None):
+    """Sort given items in place by partitioning items in range `[low...high]`
+    around a pivot item and recursively sorting each remaining sublist range.
+    TODO: Best case running time: ??? Why and under what conditions?
+    TODO: Worst case running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Check if high and low range bounds have default values (not given)
+    # TODO: Check if list or range is so small it's already sorted (base case)
+    # TODO: Partition items in-place around a pivot and get index of pivot
+    # TODO: Sort each sublist range by recursively calling quick sort
+
+
 def random_ints(count=20, min=1, max=50):
     """Return a list of `count` integers sampled uniformly at random from
     given range [`min`...`max`] with replacement (duplicates are allowed)."""
