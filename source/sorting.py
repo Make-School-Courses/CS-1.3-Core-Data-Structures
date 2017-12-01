884c6c7da4019b87bd92ff0568e0aa02f361f32e
Alan Davis <neptunius@gmail.com>
2017-12-01 14:56:32 -0800

    Add counting sort and bucket sort to sorting algorithms template

--- source/sorting.py
+++ source/sorting.py
@@ -95,6 +95,31 @@ def quick_sort(items, low=None, high=None):
     # TODO: Sort each sublist range by recursively calling quick sort
 
 
+def counting_sort(numbers):
+    """Sort given numbers (integers) by counting occurrences of each number,
+    then looping over counts and copying that many numbers into output list.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Find range of given numbers (minimum and maximum integer values)
+    # TODO: Create list of counts with a slot for each number in input range
+    # TODO: Loop over given numbers and increment each number's count
+    # TODO: Loop over counts and append that many numbers into output list
+    # FIXME: Improve this to mutate input instead of creating new output list
+
+
+def bucket_sort(numbers, num_buckets=10):
+    """Sort given numbers by distributing into buckets representing subranges,
+    sorting each bucket, and combining contents of all buckets in sorted order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Find range of given numbers (minimum and maximum integer values)
+    # TODO: Create list of buckets to store numbers in subranges of input range
+    # TODO: Loop over given numbers and place each item in appropriate bucket
+    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
+    # TODO: Loop over buckets and append each bucket's numbers into output list
+    # FIXME: Improve this to mutate input instead of creating new output list
+
+
 def random_ints(count=20, min=1, max=50):
     """Return a list of `count` integers sampled uniformly at random from
     given range [`min`...`max`] with replacement (duplicates are allowed)."""
