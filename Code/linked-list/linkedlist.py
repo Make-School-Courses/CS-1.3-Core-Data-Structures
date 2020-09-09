e57ea641d530ce0f037f5add2bca31c920aafdbc
jess-lemur <jess.dahmen@makeschool.com>
2020-09-09 12:51:53 -0700

    update starter code

--- Code/linked-list/linkedlist.py
+++ Code/linked-list/linkedlist.py
@@ -7,36 +7,20 @@ class Node(object):
         self.data = data
         self.next = None
 
-    def __repr__(self):
-        """Return a string representation of this node."""
-        return 'Node({!r})'.format(self.data)
-
 
 class LinkedList(object):
 
-    def __init__(self, iterable=None):
+    def __init__(self, item_list=None):
         """Initialize this linked list and append the given items, if any."""
         self.head = None  # First node
         self.tail = None  # Last node
-        self.size = 0  # Number of nodes
-        # Append the given items
-        if iterable is not None:
-            for item in iterable:
+
+        if item_list is not None:
+            for item in item_list:
                 self.append(item)
 
-    def __str__(self):
-        """Return a formatted string representation of this linked list."""
-        items = ['({!r})'.format(item) for item in self.items()]
-        return '[{}]'.format(' -> '.join(items))
-
-    def __repr__(self):
-        """Return a string representation of this linked list."""
-        return 'LinkedList({!r})'.format(self.items())
-
     def items(self):
-        """Return a list of all items in this linked list.
-        Best and worst case running time: Theta(n) for n items in the list
-        because we always need to loop through all n nodes."""
+        """Return a list of all items in this linked list."""
         # Create an empty list of results
         result = []  # Constant time to create a new list
         # Start at the head node
@@ -52,11 +36,13 @@ class LinkedList(object):
 
     def is_empty(self):
         """Return True if this linked list is empty, or False."""
-        return self.head is None
+        if self.head is None:
+          return True
+        else:
+          return False
 
     def length(self):
-        """Return the length of this linked list by traversing its nodes.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+        """Return the length of this linked list by traversing its nodes."""
         # Node counter initialized to zero
         node_count = 0
         # Start at the head node
@@ -70,29 +56,9 @@ class LinkedList(object):
         # Now node_count contains the number of nodes
         return node_count
 
-    def get_at_index(self, index):
-        """Return the item at the given index in this linked list, or
-        raise ValueError if the given index is out of range of the list size.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
-        # Check if the given index is out of range and if so raise an error
-        if not (0 <= index < self.size):
-            raise ValueError('List index out of range: {}'.format(index))
-        # TODO: Find the node at the given index and return its data
-
-    def insert_at_index(self, index, item):
-        """Insert the given item at the given index in this linked list, or
-        raise ValueError if the given index is out of range of the list size.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
-        # Check if the given index is out of range and if so raise an error
-        if not (0 <= index <= self.size):
-            raise ValueError('List index out of range: {}'.format(index))
-        # TODO: Find the node before the given index and insert item after it
-
     def append(self, item):
         """Insert the given item at the tail of this linked list.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+      """
         # Create a new node to hold the given item
         new_node = Node(item)
         # Check if this linked list is empty
@@ -107,7 +73,7 @@ class LinkedList(object):
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+        """
         # Create a new node to hold the given item
         new_node = Node(item)
         # Check if this linked list is empty
@@ -121,10 +87,7 @@ class LinkedList(object):
         self.head = new_node
 
     def find(self, quality):
-        """Return an item from this linked list satisfying the given quality.
-        Best case running time: Omega(1) if item is near the head of the list.
-        Worst case running time: O(n) if item is near the tail of the list or
-        not present and we need to loop through all n nodes in the list."""
+        """Return an item from this linked list satisfying the given quality."""
         # Start at the head node
         node = self.head  # Constant time to assign a variable reference
         # Loop until the node is None, which is one node too far past the tail
@@ -138,19 +101,10 @@ class LinkedList(object):
         # We never found data satisfying quality, but have to return something
         return None  # Constant time to return None
 
-    def replace(self, old_item, new_item):
-        """Replace the given old_item in this linked list with given new_item
-        using the same node, or raise ValueError if old_item is not found.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
-        # TODO: Find the node containing the given old_item and replace its
-        # data with new_item, without creating a new node object
-        pass
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+      """
         # Start at the head node
         node = self.head
         # Keep track of the node before the one containing the given item
@@ -192,41 +146,3 @@ class LinkedList(object):
         else:
             # Otherwise raise an error to tell the user that delete has failed
             raise ValueError('Item not found: {}'.format(item))
-
-
-def test_linked_list():
-    ll = LinkedList()
-    print(ll)
-
-    print('Appending items:')
-    ll.append('A')
-    print(ll)
-    ll.append('B')
-    print(ll)
-    ll.append('C')
-    print(ll)
-    print('head: {}'.format(ll.head))
-    print('tail: {}'.format(ll.tail))
-    print('size: {}'.format(ll.size))
-    print('length: {}'.format(ll.length()))
-
-    print('Getting items by index:')
-    for index in range(ll.size):
-        item = ll.get_at_index(index)
-        print('get_at_index({}): {!r}'.format(index, item))
-
-    print('Deleting items:')
-    ll.delete('B')
-    print(ll)
-    ll.delete('C')
-    print(ll)
-    ll.delete('A')
-    print(ll)
-    print('head: {}'.format(ll.head))
-    print('tail: {}'.format(ll.tail))
-    print('size: {}'.format(ll.size))
-    print('length: {}'.format(ll.length()))
-
-
-if __name__ == '__main__':
-    test_linked_list()
--- Code/linked-list/linkedlist_test.py
+++ Code/linked-list/linkedlist_test.py
@@ -19,13 +19,11 @@ class LinkedListTest(unittest.TestCase):
         ll = LinkedList()
         assert ll.head is None
         assert ll.tail is None
-        assert ll.size == 0
 
     def test_init_with_list(self):
         ll = LinkedList(['A', 'B', 'C'])
         assert ll.head.data == 'A'  # first item
         assert ll.tail.data == 'C'  # last item
-        assert ll.size == 3
 
     def test_items(self):
         ll = LinkedList()
@@ -55,86 +53,29 @@ class LinkedListTest(unittest.TestCase):
         ll.delete('A')
         assert ll.length() == 0
 
-    def test_size(self):
-        ll = LinkedList()
-        assert ll.size == 0
-        # append and prepend operations increment size
-        ll.append('B')
-        assert ll.size == 1
-        ll.prepend('A')
-        assert ll.size == 2
-        ll.append('C')
-        assert ll.size == 3
-        # delete operations decrement size
-        ll.delete('B')
-        assert ll.size == 2
-        ll.delete('C')
-        assert ll.size == 1
-        ll.delete('A')
-        assert ll.size == 0
-
-    def test_get_at_index(self):
-        ll = LinkedList(['A', 'B', 'C'])
-        assert ll.get_at_index(0) == 'A'  # head item
-        assert ll.get_at_index(1) == 'B'  # middle item
-        assert ll.get_at_index(2) == 'C'  # tail item
-        with self.assertRaises(ValueError):
-            ll.get_at_index(3)  # index too high
-        with self.assertRaises(ValueError):
-            ll.get_at_index(-1)  # index too low
-
-    def test_insert_at_index(self):
-        ll = LinkedList()
-        ll.insert_at_index(0, 'B')  # append('B')
-        assert ll.head.data == 'B'  # new head (at index 0)
-        assert ll.tail.data == 'B'  # new tail (at index 0)
-        assert ll.size == 1
-        ll.insert_at_index(0, 'A')  # prepend('A')
-        assert ll.head.data == 'A'  # new head (at index 0)
-        assert ll.tail.data == 'B'  # unchanged (now at index 1)
-        assert ll.size == 2
-        ll.insert_at_index(2, 'D')  # append('D')
-        assert ll.head.data == 'A'  # unchanged (at index 0)
-        assert ll.tail.data == 'D'  # new tail (now at index 2)
-        assert ll.size == 3
-        ll.insert_at_index(2, 'C')  # insert 'C' between 'B' and 'D'
-        assert ll.head.data == 'A'  # unchanged (at index 0)
-        assert ll.tail.data == 'D'  # unchanged (now at index 3)
-        assert ll.size == 4
-        with self.assertRaises(ValueError):
-            ll.insert_at_index(5, 'X')  # index too high
-        with self.assertRaises(ValueError):
-            ll.insert_at_index(-1, 'Y')  # index too low
-
     def test_append(self):
         ll = LinkedList()
         ll.append('A')
         assert ll.head.data == 'A'  # new head
         assert ll.tail.data == 'A'  # new tail
-        assert ll.size == 1
         ll.append('B')
         assert ll.head.data == 'A'  # unchanged
         assert ll.tail.data == 'B'  # new tail
-        assert ll.size == 2
         ll.append('C')
         assert ll.head.data == 'A'  # unchanged
         assert ll.tail.data == 'C'  # new tail
-        assert ll.size == 3
 
     def test_prepend(self):
         ll = LinkedList()
         ll.prepend('C')
         assert ll.head.data == 'C'  # new head
         assert ll.tail.data == 'C'  # new head
-        assert ll.size == 1
         ll.prepend('B')
         assert ll.head.data == 'B'  # new head
         assert ll.tail.data == 'C'  # unchanged
-        assert ll.size == 2
         ll.prepend('A')
         assert ll.head.data == 'A'  # new head
         assert ll.tail.data == 'C'  # unchanged
-        assert ll.size == 3
 
     def test_find(self):
         ll = LinkedList(['A', 'B', 'C'])
@@ -143,37 +84,18 @@ class LinkedListTest(unittest.TestCase):
         assert ll.find(lambda item: item > 'B') == 'C'
         assert ll.find(lambda item: item == 'X') is None
 
-    def test_replace(self):
-        ll = LinkedList(['A', 'B', 'C'])
-        ll.replace('A', 'D')
-        assert ll.head.data == 'D'  # new head
-        assert ll.tail.data == 'C'  # unchanged
-        assert ll.size == 3
-        ll.replace('B', 'E')
-        assert ll.head.data == 'D'  # unchanged
-        assert ll.tail.data == 'C'  # unchanged
-        assert ll.size == 3
-        ll.replace('C', 'F')
-        assert ll.head.data == 'D'  # unchanged
-        assert ll.tail.data == 'F'  # new tail
-        assert ll.size == 3
-        with self.assertRaises(ValueError):
-            ll.replace('X', 'Y')  # item not in list
 
     def test_delete(self):
         ll = LinkedList(['A', 'B', 'C'])
         ll.delete('A')
         assert ll.head.data == 'B'  # new head
         assert ll.tail.data == 'C'  # unchanged
-        assert ll.size == 2
         ll.delete('C')
         assert ll.head.data == 'B'  # unchanged
         assert ll.tail.data == 'B'  # new tail
-        assert ll.size == 1
         ll.delete('B')
         assert ll.head is None  # new head
         assert ll.tail is None  # new head
-        assert ll.size == 0
         with self.assertRaises(ValueError):
             ll.delete('X')  # item not in list
 
